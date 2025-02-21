431812a4a97820e93ab1b7d0400f4e61fa3febd5
Jose Mendoza <mendozamendoza1998@outlook.es>
2025-02-20 18:12:19 -0600

    Inyeccion de pendencia

--- L01_2020CM606_2023LG651/Program.cs
+++ L01_2020CM606_2023LG651/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Inyección por dependencia del string de conexión al contexto
+builder.Services.AddDbContext<AplicattionDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("EquiposDbConnection")
+));
 
 var app = builder.Build();
 
