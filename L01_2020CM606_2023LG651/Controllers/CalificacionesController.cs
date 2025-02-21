893f4e68b9014fc97f5b70f08809e09525d1a20d
nelsonlopez500 <nelson.lopez@catolica.edu.sv>
2025-02-20 18:07:53 -0600

    Agregar controladores para Calificaciones, Comentarios y Usuarios

---
+++ L01_2020CM606_2023LG651/Controllers/CalificacionesController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L01_2020CM606_2023LG651.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalificacionesController : ControllerBase
+    {
+    }
+}
